2e05fd16dd186cee5d3b3d5e6562c6f4c43749c7
frankc60 <frankc60@gmail.com>
2019-02-03 14:00:22 +0100

    working with express sessions

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-//index.js
+const express = require('express');
+const session = require('express-session');
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/scripts', express.static(path.join(__dirname, 'public/scripts')));
+
+app.use(session({
+    secret: "Shh, its a secret!"
+}));
+
+/* app.get("/code", (req, res, next) => {
+    console.log("code " + req.params.name);
+    next();
+}); */
+
+app.get("/code", (req, res, next) => {
+
+    if (req.session.page_views) {
+        req.session.page_views++;
+        res.send({ name: "firebase", visits: req.session.page_views, passedData: req.query.name });
+    } else {
+        req.session.page_views = 1;
+        res.send({
+            name: "firebase",
+            visits: req.session.page_views,
+            passedData: req.query.name
+        });
+    }
+  //  res.send({ name: "firebase", calls: counter });    
+})
+
+app.get('/spice', (req, res) => res.send('Hello World!'))
+
+
+app.listen(port, () => console.log(`firebase app listening on port ${port}!`))
